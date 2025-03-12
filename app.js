f3dab7c05418b0565775b3b49506f5fc59401ce3
.dyn <dhyanjain341988@gmail.com>
2025-03-12 16:44:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+mongoose.connect('mongodb://localhost:27017/your_database_name', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected successfully'))
+.catch(err => console.error('MongoDB connection error:', err)); 
